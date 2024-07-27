8bc24f226c525abdb386ef59c0b2b5a77b72568b
Mark Adrian Mansilla <162296706+mamansilla@users.noreply.github.com>
2024-07-27 14:10:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFtYW5zaWxsYSIsImEiOiJjbHozbTB3bWYwMmM4MnJvbGJzZXdoMmdjIn0.gsO8s_-tnGzvhcWYo3zqJg';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
